c0738d2636ba121abf72f062f2ef9324ff896f5b
Nicolas Mansilla <nlmansilla89@gmail.com>
2024-09-24 23:35:24 +0100

    First commit. Setup project, Index page, header and trend components.

---
+++ components/app-header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="flex justify-between items-center mt-10">
+        <NuxtLink to="/" class="text-xl font-bold">
+            Finance Tracker
+        </NuxtLink>
+        <div>
+            <template>
+                <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+            </template>
+        </div>
+    </header>
+</template>
